374422bf63fad60cf7633c3a78c25614e625c9c1
asathoor <per@multimusen.dk>
2018-02-08 14:00:46 +0100

    slut på undervisning

--- js/myScript.js
+++ js/myScript.js
@@ -5,13 +5,22 @@
  **/
 
 // variabler
+var vand = "vand"; // tekststreng
+var vandIgen = 'vand';
+var vandAahNej = "vand smager da \"rigtigt godt\"";
+
 var tal = 111;
 var erDetRigtigt = false;
+var julemandenFindes = true;
 var enStreng = "hej verden";
 
 // klister (+)
+var total = vand + " " + vandAahNej;
+
 // kommentarer
 // regning
+var talMere = 999;
+
 // datoer
 // lister (scratch: data)
 // hvis ... så ... (scratch: operators)
@@ -21,8 +30,14 @@ var enStreng = "hej verden";
 // noget her
 
 /* se resultatet af din kode i browserens udviklings-værktøj */
-console.log( enStreng  );
+console.log( "sum = " + tal + talMere  );
+console.log( vand  );
+
 // skriv i browservinduet
+document.write("<h1 style='color:red'>" 
+	+ "Mojn, mojn do! </h1><p>" 
+	+ vandAahNej 
+	+ "</p>");
 
 /* se resultatet af din kode, der hvor scriptet ligger i koden */
 
