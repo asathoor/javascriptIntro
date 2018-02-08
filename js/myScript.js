e19c5664868f630a5d460ab64cfb40003f3db5ee
asathoor <per@multimusen.dk>
2018-02-08 09:32:32 +0100

    d2 js renset

--- js/myScript.js
+++ js/myScript.js
@@ -6,27 +6,12 @@
 
 // variabler
 var tal = 111;
-
-// booleans
 var erDetRigtigt = false;
-var erDetRigtigt = true;
-
-// en streng (noget tekst)
-var veritas = 'Julemanden "findes", og jorden er flad.';
+var enStreng = "hej verden";
 
 // klister (+)
-var and = "<h1> Mit navn er Anders. Anders And.</h1>";
-var gaas = "Madam Gaas";
-var uha = "De Nattergale";
-
-// datatyper
-var x = 0;
-
 // kommentarer
 // regning
-var ind = 2345;
-var ud = 9999;
-
 // datoer
 // lister (scratch: data)
 // hvis ... så ... (scratch: operators)
@@ -36,8 +21,9 @@ var ud = 9999;
 // noget her
 
 /* se resultatet af din kode i browserens udviklings-værktøj */
-console.log( "sum:" + (ind - ud) * 12);
+console.log( enStreng  );
 // skriv i browservinduet
-document.write( and ); 
+
+/* se resultatet af din kode, der hvor scriptet ligger i koden */
 
 // næste gang: events.
